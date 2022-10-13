800f4df43502799f86d03ac41c6aa1daf5191706
lum1narie <sn.lum.fallenrose.aol@gmail.com>
2022-10-13 14:11:31 +0900

    :shirt: warningを消した

--- main.cpp
+++ main.cpp
@@ -9,12 +9,15 @@ using array_mul_func = void (*)(float[ARRAY_SIZ][ARRAY_SIZ], float[ARRAY_SIZ],
 
 using array_preprocess_func = void (*)(float[ARRAY_SIZ][ARRAY_SIZ]);
 
-void do_nothing_preprocess(float A[ARRAY_SIZ][ARRAY_SIZ]) { return; }
+void do_nothing_preprocess(float A[ARRAY_SIZ][ARRAY_SIZ]
+                           __attribute__((unused))) {
+  return;
+}
 
 void naive_array_mul(float A[ARRAY_SIZ][ARRAY_SIZ], float x[ARRAY_SIZ],
                      float y[ARRAY_SIZ]) {
-  for (auto i = 0; i < ARRAY_SIZ; ++i) {
-    for (auto j = 0; j < ARRAY_SIZ; ++j) {
+  for (size_t i = 0; i < ARRAY_SIZ; ++i) {
+    for (size_t j = 0; j < ARRAY_SIZ; ++j) {
       y[i] += A[i][j] * x[j];
     }
   }
@@ -23,8 +26,8 @@ void naive_array_mul(float A[ARRAY_SIZ][ARRAY_SIZ], float x[ARRAY_SIZ],
 
 void reversed_idx_array_mul(float A[ARRAY_SIZ][ARRAY_SIZ], float x[ARRAY_SIZ],
                             float y[ARRAY_SIZ]) {
-  for (auto j = 0; j < ARRAY_SIZ; ++j) {
-    for (auto i = 0; i < ARRAY_SIZ; ++i) {
+  for (size_t j = 0; j < ARRAY_SIZ; ++j) {
+    for (size_t i = 0; i < ARRAY_SIZ; ++i) {
       y[i] += A[i][j] * x[j];
     }
   }
@@ -37,13 +40,13 @@ double measure_ms_array_mul(array_mul_func mul_f, array_preprocess_func pre_f,
   float x[rep_n][ARRAY_SIZ], y[rep_n][ARRAY_SIZ];
 
   // initialize arrays
-  for (int i = 0; i < ARRAY_SIZ; ++i) {
-    for (int j = 0; j < ARRAY_SIZ; ++j) {
+  for (size_t i = 0; i < ARRAY_SIZ; ++i) {
+    for (size_t j = 0; j < ARRAY_SIZ; ++j) {
       A[i][j] = float(i + j);
     }
   }
-  for (int i = 0; i < rep_n; ++i) {
-    for (int j = 0; j < ARRAY_SIZ; ++j) {
+  for (size_t i = 0; i < rep_n; ++i) {
+    for (size_t j = 0; j < ARRAY_SIZ; ++j) {
       x[i][j] = float(i - j);
       y[i][j] = 0;
     }
@@ -58,7 +61,7 @@ double measure_ms_array_mul(array_mul_func mul_f, array_preprocess_func pre_f,
   pre_f(A);
 
   // calc queue
-  for (auto i = 0; i < rep_n; ++i) {
+  for (size_t i = 0; i < rep_n; ++i) {
     mul_f(A, x[i], y[i]);
   }
   end = std::chrono::system_clock::now();
@@ -83,20 +86,20 @@ bool verify_array_mul(array_mul_func mul_f_1, array_preprocess_func pre_f_1,
   std::uniform_real_distribution<float> dist(-VERIFY_VAL_RANGE,
                                              VERIFY_VAL_RANGE);
 
-  for (auto i = 0; i < rep_n; ++i) {
+  for (size_t i = 0; i < rep_n; ++i) {
     float A1[ARRAY_SIZ][ARRAY_SIZ], A2[ARRAY_SIZ][ARRAY_SIZ];
     float x[ARRAY_SIZ];
     float y1[ARRAY_SIZ], y2[ARRAY_SIZ];
 
     // initialize arrays
-    for (int i = 0; i < ARRAY_SIZ; ++i) {
-      for (int j = 0; j < ARRAY_SIZ; ++j) {
+    for (size_t i = 0; i < ARRAY_SIZ; ++i) {
+      for (size_t j = 0; j < ARRAY_SIZ; ++j) {
         float a = dist(engine);
         A1[i][j] = a;
         A2[i][j] = a;
       }
     }
-    for (int i = 0; i < ARRAY_SIZ; ++i) {
+    for (size_t i = 0; i < ARRAY_SIZ; ++i) {
       x[i] = dist(engine);
       y1[i] = 0;
       y2[i] = 0;
@@ -110,7 +113,7 @@ bool verify_array_mul(array_mul_func mul_f_1, array_preprocess_func pre_f_1,
     mul_f_2(A2, x, y2);
 
     // check
-    for (int i = 0; i < ARRAY_SIZ; ++i) {
+    for (size_t i = 0; i < ARRAY_SIZ; ++i) {
       float difference =
           (y2[i] == 0) ? std::abs(y1[i]) : std::abs(1 - y1[i] / y2[i]);
       bool is_different = difference > VERIFY_EPS;
